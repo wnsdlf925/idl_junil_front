7597055026e82f46d3914f2e664efe453c86d9c6
wnsdlf925 <62425882+wnsdlf925@users.noreply.github.com>
2021-05-07 17:54:49 +0900

    2021.05.07 새로고침 오류 해결

--- myfront/src/app/app-routing.module.ts
+++ myfront/src/app/app-routing.module.ts
@@ -67,7 +67,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{enableTracing:false, onSameUrlNavigation: 'reload'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
