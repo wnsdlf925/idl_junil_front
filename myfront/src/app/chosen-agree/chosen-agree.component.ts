ed63f5affd5794c9b13c9de2311893802783cf98
wnsdlf925 <62425882+wnsdlf925@users.noreply.github.com>
2021-05-07 14:12:38 +0900

    2021.05.07 체크박스 실험

--- myfront/src/app/chosen-agree/chosen-agree.component.ts
+++ myfront/src/app/chosen-agree/chosen-agree.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-chosen-agree',
@@ -7,7 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChosenAgreeComponent implements OnInit {
 
-  constructor() { }
+
+
+  chosen1 : boolean = false;
+  chosen2 : boolean = false;
+
+
+
+
+  check1():void{
+    this.chosen1=!this.chosen1;
+    console.log(this.chosen1)
+    
+  }
+  check2():void{
+    this.chosen2=!this.chosen2;
+    console.log(this.chosen2)
+    
+  }
+ 
+  agree():void{
+    if(this.chosen1 ==true && this.chosen2==true){
+      this.router.navigate(["/member/join"]);
+      
+    }else{
+      alert("필수란에 체크해야함")
+      
+    }
+  }
+  
+
+
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
   }
