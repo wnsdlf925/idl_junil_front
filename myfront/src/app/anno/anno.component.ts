ab190042d4e0afc18d1c7b71e69db97b68cb5022
wnsdlf925 <62425882+wnsdlf925@users.noreply.github.com>
2021-05-11 19:07:27 +0900

    2021.05.11 api연결

--- myfront/src/app/anno/anno.component.ts
+++ myfront/src/app/anno/anno.component.ts
@@ -12,20 +12,68 @@ import { Observable } from 'rxjs';
 })
 export class AnnoComponent implements OnInit {
 
-  list:any = [];
   
   constructor(public http:HttpClient) { }
-
   
   
+  list:any = [];
+  listTitle:any = [];
+  totalPageNum: number=0; //페이지 개수
+  pageArr: number[] = [1,2,3,4,5];
+  paramPageNum:number = 1; //api pageNum 파라미터
+  totalNum:number = 0;//총 게시물 개수
+  samePage:boolean=false;
   
   ngOnInit(): void {
     
-    this.http.get<any>('http://localhost:3000/board/anno?pageNum=1').subscribe(data=>{
+    this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.paramPageNum).subscribe(data=>{
       this.list = data;
-      console.log("this.list: "+this.list)
+      this.listTitle = data.result[0];
+      this.totalPageNum=data.postNum;
+      this.totalNum=data.result[1][0].num
+      console.log("pageNum:"+this.totalPageNum)
+      
+      
     })
-   
+    
+    
   }
+  
+  
+  
+  changePage(a:number){
+    this.paramPageNum = a;
+    this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.paramPageNum).subscribe(data=>{
+      this.list = data;
+      this.listTitle = data.result[0];
+      this.totalPageNum=data.postNum;
+      console.log("pageNum:"+this.totalPageNum)
+      
+    })
+  
+  }
+  
+
+  pagePoint(a:number){
+    console.log("paramPageNum:"+this.paramPageNum)
+    console.log("a:"+a)
+    console.log("----------------------------------------------")
+    if(this.paramPageNum == a){
+      this.samePage=true;
+      return this.samePage
+    }else{
+      this.samePage=false;
+      return this.samePage
+    }
+  }
+
+
+
+
+
+
+
+
+
 
 }
