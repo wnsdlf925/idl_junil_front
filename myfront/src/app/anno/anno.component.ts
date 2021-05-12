652230bd508e1f7f1460bbb7c9567ac592d623da
wnsdlf925 <62425882+wnsdlf925@users.noreply.github.com>
2021-05-12 19:06:51 +0900

    2021.05.12 페이징 작업중

--- myfront/src/app/anno/anno.component.ts
+++ myfront/src/app/anno/anno.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient} from '@angular/common/http'; 
-import {AnnoService} from './anno.component.service';
-import { take } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+
 
 
 @Component({
@@ -11,53 +9,110 @@ import { Observable } from 'rxjs';
   styleUrls: ['./anno.component.css']
 })
 export class AnnoComponent implements OnInit {
-
   
   constructor(public http:HttpClient) { }
+
   
   
   list:any = [];
   listTitle:any = [];
-  totalPageNum: number=0; //페이지 개수
+  totalPageNum: number = 0; //페이지 개수
   pageArr: number[] = [1,2,3,4,5];
   paramPageNum:number = 1; //api pageNum 파라미터
   totalNum:number = 0;//총 게시물 개수
+  nextPageNum:number = 2; //페이지 5쪽 건너뛰기 
   samePage:boolean=false;
+  pageJump:number = 0;
   
   ngOnInit(): void {
     
-    this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.paramPageNum).subscribe(data=>{
+    this.http.get<any>(`http://localhost:3000/board/anno?pageNum=${this.paramPageNum}`).subscribe(data=>{
       this.list = data;
       this.listTitle = data.result[0];
       this.totalPageNum=data.postNum;
       this.totalNum=data.result[1][0].num
-      console.log("pageNum:"+this.totalPageNum)
-      
+      // console.log("pageNum:"+this.totalPageNum)
+      console.log("onInit")
       
     })
-    
-    
   }
-  
-  
-  
   changePage(a:number){
     this.paramPageNum = a;
     this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.paramPageNum).subscribe(data=>{
       this.list = data;
       this.listTitle = data.result[0];
       this.totalPageNum=data.postNum;
-      console.log("pageNum:"+this.totalPageNum)
+      
       
     })
-  
   }
-  
+  nextPage(){
+    this.pageJump = (5*(this.nextPageNum-1))+1
+    if(this.totalPageNum > this.pageJump){
+      for( let i = 0; i<5;i++){
+        this.pageArr[i] += 5; 
+        console.log("this.pageArr[i] :"+this.pageArr[i] )
+      } 
+      this.nextPageNum++;
+      this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.pageJump).subscribe(data=>{
+        this.list = data;
+        this.listTitle = data.result[0];
+        this.totalPageNum=data.postNum;
+        this.paramPageNum=this.pageJump;
+        console.log("pageJump: "+this.pageJump)
+        
+      })
+    }else{
+      this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.totalPageNum).subscribe(data=>{
+        this.list = data;
+        this.listTitle = data.result[0];
+        this.totalPageNum=data.postNum;
+        this.paramPageNum=this.totalPageNum;
+        
+        console.log("over")
+        
+      })
+    }
+  }
+
+  backPage(){
+    this.pageJump = this.pageJump-5;
+    if(1 <= this.pageJump){
+      for( let i = 0; i<5;i++){
+        this.pageArr[i] -= 5; 
+        console.log("this.pageArr[i] :"+this.pageArr[i] )
+      } 
+      this.nextPageNum--;
+      this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.pageJump).subscribe(data=>{
+        this.list = data;
+        this.listTitle = data.result[0];
+        this.totalPageNum=data.postNum;
+        this.paramPageNum=this.pageJump;
+        console.log("pageJump: "+this.pageJump)
+        
+      })
+    }else{
+      // this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.totalPageNum).subscribe(data=>{
+      //   this.list = data;
+      //   this.listTitle = data.result[0];
+      //   this.totalPageNum=data.postNum;
+      //   this.paramPageNum=this.totalPageNum;
+        
+        
+      // })
+      console.log("over")
+    }
+  }
+  backPageAble(){
+    if(this.nextPageNum>1){
+      return true;
+    }else{
+      return false;
+    }
+  }
 
   pagePoint(a:number){
-    console.log("paramPageNum:"+this.paramPageNum)
-    console.log("a:"+a)
-    console.log("----------------------------------------------")
+   
     if(this.paramPageNum == a){
       this.samePage=true;
       return this.samePage
