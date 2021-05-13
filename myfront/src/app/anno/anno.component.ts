a88da88f7b1711ae31196f6c10f5a643e0a0b7b5
wnsdlf925 <62425882+wnsdlf925@users.noreply.github.com>
2021-05-13 18:53:00 +0900

    2021.05.13 페이징 구현 1~5제외

--- myfront/src/app/anno/anno.component.ts
+++ myfront/src/app/anno/anno.component.ts
@@ -14,15 +14,21 @@ export class AnnoComponent implements OnInit {
 
   
   
-  list:any = [];
+  list:any = []; //api 받아오기
   listTitle:any = [];
   totalPageNum: number = 0; //페이지 개수
-  pageArr: number[] = [1,2,3,4,5];
+  oriPageArr: any[] = [1,2,3,4,5];//원래 페이지 갯수
+  pageArr: any[] = [1,2,3,4,5];
   paramPageNum:number = 1; //api pageNum 파라미터
   totalNum:number = 0;//총 게시물 개수
-  nextPageNum:number = 2; //페이지 5쪽 건너뛰기 
-  samePage:boolean=false;
-  pageJump:number = 0;
+  nextPageNum:number = 1; //페이지 5쪽 건너뛰기 
+  samePage:boolean=false; //클릭한 페이지 보라색 되게끔
+  pageJump:number = 1; //다음쪽
+  lastPage:number = 0; //마지막페이지 배열
+  pageMath:number = 0;
+  lastPageArr: any =[];
+  disLeft:any = "none" ; 
+  disRight:any = "inline-table" ; 
   
   ngOnInit(): void {
     
@@ -31,7 +37,12 @@ export class AnnoComponent implements OnInit {
       this.listTitle = data.result[0];
       this.totalPageNum=data.postNum;
       this.totalNum=data.result[1][0].num
-      // console.log("pageNum:"+this.totalPageNum)
+      this.pageMath = Math.floor(this.totalPageNum/5)
+      this.lastPage = (this.pageMath)*5+1;
+      for(let i=1; i<=this.totalPageNum%5; i++){
+        this.lastPageArr.push(this.pageMath*5+i);
+      }
+      console.log(" this.lastPage: "+ this.lastPage)
       console.log("onInit")
       
     })
@@ -46,63 +57,73 @@ export class AnnoComponent implements OnInit {
       
     })
   }
+
+
+
+
+
   nextPage(){
-    this.pageJump = (5*(this.nextPageNum-1))+1
-    if(this.totalPageNum > this.pageJump){
-      for( let i = 0; i<5;i++){
-        this.pageArr[i] += 5; 
-        console.log("this.pageArr[i] :"+this.pageArr[i] )
-      } 
-      this.nextPageNum++;
-      this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.pageJump).subscribe(data=>{
-        this.list = data;
-        this.listTitle = data.result[0];
-        this.totalPageNum=data.postNum;
-        this.paramPageNum=this.pageJump;
-        console.log("pageJump: "+this.pageJump)
-        
-      })
-    }else{
-      this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.totalPageNum).subscribe(data=>{
-        this.list = data;
-        this.listTitle = data.result[0];
-        this.totalPageNum=data.postNum;
-        this.paramPageNum=this.totalPageNum;
-        
-        console.log("over")
-        
-      })
+    
+    this.pageJump = (5*(this.nextPageNum))+1
+
+    if(this.lastPage > this.pageJump){
+      this.disLeft = "inline-table" ; 
+
+      for( let i = 0; i< this.pageArr.length;i++){
+            this.pageArr[i] = this.pageJump+i; 
+            this.oriPageArr[i] = this.pageJump+i;
+            console.log("this.pageArr[i] :"+this.pageArr[i] )
+          } 
+     
+          this.getAnnoApi()
+      this.nextPageNum++
+
+    }else if(this.lastPage == this.pageJump){
+      this.disRight = "none";
+      this.pageArr=this.lastPageArr; 
+      this.getAnnoApi()
     }
   }
 
+
+
+  
   backPage(){
-    this.pageJump = this.pageJump-5;
-    if(1 <= this.pageJump){
-      for( let i = 0; i<5;i++){
-        this.pageArr[i] -= 5; 
+    
+    if(this.lastPage  == this.pageJump){
+      this.disRight = "inline-table" ; 
+      this.pageJump -=5;
+      this.pageArr=this.oriPageArr; 
+      for( let i = 0; i< this.pageArr.length;i++){
+        this.pageArr[i] = this.pageJump+i; 
+        this.oriPageArr[i] = this.pageJump+i;
         console.log("this.pageArr[i] :"+this.pageArr[i] )
       } 
-      this.nextPageNum--;
-      this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.pageJump).subscribe(data=>{
-        this.list = data;
-        this.listTitle = data.result[0];
-        this.totalPageNum=data.postNum;
-        this.paramPageNum=this.pageJump;
-        console.log("pageJump: "+this.pageJump)
-        
-      })
+      this.getAnnoApi()
+      
+      
+    }else if(1 == this.pageJump){
+      this.getAnnoApi()
+      
+     
     }else{
-      // this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.totalPageNum).subscribe(data=>{
-      //   this.list = data;
-      //   this.listTitle = data.result[0];
-      //   this.totalPageNum=data.postNum;
-      //   this.paramPageNum=this.totalPageNum;
-        
-        
-      // })
-      console.log("over")
+      this.disRight = "inline-table" ; 
+      this.pageJump -=5;
+      for( let i = 0; i< this.pageArr.length;i++){
+        this.pageArr[i] = this.pageJump+i; 
+        this.oriPageArr[i] = this.pageJump+i;
+        console.log("this.pageArr[i] :"+this.pageArr[i] )
+      } 
+      this.getAnnoApi()
+      
+      --this.nextPageNum
+      if(1 == this.pageJump){
+        this.disLeft = "none";
+      }
     }
   }
+  
+
   backPageAble(){
     if(this.nextPageNum>1){
       return true;
@@ -123,7 +144,17 @@ export class AnnoComponent implements OnInit {
   }
 
 
-
+  getAnnoApi():void{
+    this.http.get<any>('http://localhost:3000/board/anno?pageNum='+this.pageJump).subscribe(data=>{
+      this.list = data;
+      this.listTitle = data.result[0];
+      this.totalPageNum=data.postNum;
+      this.paramPageNum=this.pageJump;
+      console.log("pageJump: "+this.pageJump)
+      console.log("nextPageNum: "+this.nextPageNum)
+      
+    })  
+  }
 
 
 
